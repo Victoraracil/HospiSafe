44392e4a1d3b1c1a4f7a9de1bbef68bdb7b491f4
Victor Aracil <117686993+Victoraracil@users.noreply.github.com>
2025-11-13 17:24:49 +0100

    app wpf

--- HospiSafe_Consola/HospiSafe_Consola/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Welcome to HospiSafe Console Application!");
-        // Additional code for the console application can be added here.
-    }
-}
