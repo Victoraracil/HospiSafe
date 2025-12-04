00345b49ac65719d423f288a0d0c2f8963c07d31
rubxngdev <rubengdev@gmail.com>
2025-12-04 18:41:55 +0100

    Añadida clase base Persona con validaciones, y añadidas clases Paciente, Prueba, Cita, Usuario

--- HospiSafe_WPF/HospiSafe_WPF/obj/Debug/net8.0-windows/HospiSafe_WPF.AssemblyInfo.cs
+++ HospiSafe_WPF/HospiSafe_WPF/obj/Debug/net8.0-windows/HospiSafe_WPF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HospiSafe_WPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44392e4a1d3b1c1a4f7a9de1bbef68bdb7b491f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1345858edd2802b3b64b739b99134a672e412ca0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HospiSafe_WPF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HospiSafe_WPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
