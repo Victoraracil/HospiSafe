eabea5b2b6c088cd411c1f8c983d7d008cc11712
Victor Aracil <117686993+Victoraracil@users.noreply.github.com>
2025-12-17 19:31:50 +0100

    base de datos hecha y conectada en docker

--- HospiSafe_WPF/HospiSafe_WPF/obj/Debug/net8.0-windows/App.g.i.cs
+++ HospiSafe_WPF/HospiSafe_WPF/obj/Debug/net8.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace HospiSafe_WPF {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.11.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace HospiSafe_WPF {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.11.0")]
         public static void Main() {
             HospiSafe_WPF.App app = new HospiSafe_WPF.App();
             app.InitializeComponent();
