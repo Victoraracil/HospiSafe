2a2bb4a629bff8ee6c7bf53799fad2625b15142d
rubxngdev <rubengdev@gmail.com>
2026-04-03 18:23:34 +0200

    Implementar login con autenticación mas mejora visuales y mostrar datos del usuario en main window

--- HospiSafe/HospiSafe/App.xaml.cs
+++ HospiSafe/HospiSafe/App.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Configuration;
+﻿using HospiSafe.Services;
+using System.Configuration;
 using System.Data;
+using System.Text;
 using System.Windows;
 
 namespace HospiSafe
@@ -9,6 +11,23 @@ namespace HospiSafe
     /// </summary>
     public partial class App : Application
     {
-    }
+        //POPUP CON hashes para insertarlos en sql hardcodeados, luego barajamos registro real
+        /*
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
 
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                string hash = PasswordHelper.HashPassword("1234");
+                sb.AppendLine($"-- Usuario {i}");
+                sb.AppendLine($"'{hash}',");
+            }
+
+            MessageBox.Show(sb.ToString(), "HASHES GENERADOS");
+        }
+        */
+    }
 }
---
+++ HospiSafe/HospiSafe/Services/SessionManager.cs
@@ -0,0 +1,34 @@
+﻿using HospiSafe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospiSafe.Services
+{
+    //Singleton con el usuario de la sesion para manejo de su rol y accesos
+    public static class SessionManager
+    {
+        private static Usuario? _currentUser; //usuario actual
+
+        public static event Action? CurrentUserChanged;
+
+        public static Usuario? CurrentUser
+        {
+            get
+            {
+                return _currentUser;
+            }
+
+            set
+            {
+                if (_currentUser != value)
+                {
+                    _currentUser = value;
+                    CurrentUserChanged?.Invoke();
+                }
+            }
+        }
+    }
+}
